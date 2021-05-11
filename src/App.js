eae911aa66c76c96c99e172f347f28a595eb8539
manikantha92 <manikantha92@gmail.com>
2021-05-11 16:50:13 -0400

    adding custom component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My todos</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore full React'/>
+    </div>
+  );
 }
 
 export default App;
