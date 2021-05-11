d1901a40f5e3ceb183a331b9f301438647d6a999
manikantha92 <manikantha92@gmail.com>
2021-05-11 17:41:58 -0400

    more about Modal, props and state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Master React'/>
-      <Todo text='Explore full React'/>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore full React" />
+
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
+
 function Todo(props) {
+  const [ modalIsOpen, setModalIsOpen ] =  useState(false);
+
+    function deleteHandler() {
+        setModalIsOpen(true);
+    }
+    function closeModalhandler() {
+        setModalIsOpen(false);
+    }
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+      { modalIsOpen ?  <Modal onCancel={closeModalhandler} onConfirm={closeModalhandler}/> : null}
+      { modalIsOpen ? <Backdrop onCancel={closeModalhandler}/> : null}
     </div>
   );
 }
